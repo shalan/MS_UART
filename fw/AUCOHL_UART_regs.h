a6cf0ed4a84bac07bd0b01a9ad05bd0fea122af8
Mohamed Shalan <mshalan@aucegypt.edu>
2025-05-01 19:02:03 +0300

    Metadata updates and views regeneration

--- fw/AUCOHL_UART_regs.h
+++ fw/AUCOHL_UART_regs.h
@@ -24,69 +24,150 @@
 
 */
 
-#ifndef AUCOHL_UART_H
-#define AUCOHL_UART_H
+#ifndef MS_UARTREGS_H
+#define MS_UARTREGS_H
+
+ 
+/******************************************************************************
+* Includes
+******************************************************************************/
+#include <stdint.h>
+
+/******************************************************************************
+* Macros and Constants
+******************************************************************************/
 
 #ifndef IO_TYPES
 #define IO_TYPES
-#define   __R     volatile const unsigned int
-#define   __W     volatile       unsigned int
-#define   __RW    volatile       unsigned int
+#define   __R     volatile const uint32_t
+#define   __W     volatile       uint32_t
+#define   __RW    volatile       uint32_t
 #endif
 
-#define AUCOHL_UART_CTRL_REG_EN_BIT	0
-#define AUCOHL_UART_CTRL_REG_EN_MASK	0x1
-#define AUCOHL_UART_CTRL_REG_TXEN_BIT	1
-#define AUCOHL_UART_CTRL_REG_TXEN_MASK	0x2
-#define AUCOHL_UART_CTRL_REG_RXEN_BIT	2
-#define AUCOHL_UART_CTRL_REG_RXEN_MASK	0x4
-#define AUCOHL_UART_CTRL_REG_LPEN_BIT	3
-#define AUCOHL_UART_CTRL_REG_LPEN_MASK	0x8
-#define AUCOHL_UART_CTRL_REG_GFEN_BIT	4
-#define AUCOHL_UART_CTRL_REG_GFEN_MASK	0x10
-#define AUCOHL_UART_CFG_REG_WLEN_BIT	0
-#define AUCOHL_UART_CFG_REG_WLEN_MASK	0xf
-#define AUCOHL_UART_CFG_REG_STP2_BIT	4
-#define AUCOHL_UART_CFG_REG_STP2_MASK	0x10
-#define AUCOHL_UART_CFG_REG_PARITY_BIT	5
-#define AUCOHL_UART_CFG_REG_PARITY_MASK	0xe0
-#define AUCOHL_UART_CFG_REG_TIMEOUT_BIT	8
-#define AUCOHL_UART_CFG_REG_TIMEOUT_MASK	0x3f00
-#define AUCOHL_UART_FIFOCTRL_REG_TXLT_BIT	0
-#define AUCOHL_UART_FIFOCTRL_REG_TXLT_MASK	0xf
-#define AUCOHL_UART_FIFOCTRL_REG_RXLT_BIT	8
-#define AUCOHL_UART_FIFOCTRL_REG_RXLT_MASK	0xf00
-#define AUCOHL_UART_FIFOS_REG_RXL_BIT	0
-#define AUCOHL_UART_FIFOS_REG_RXL_MASK	0xf
-#define AUCOHL_UART_FIFOS_REG_TXL_BIT	8
-#define AUCOHL_UART_FIFOS_REG_TXL_MASK	0xf00
+#define MS_UART_RXDATA_REG_RXDATA_BIT	((uint32_t)0)
+#define MS_UART_RXDATA_REG_RXDATA_MASK	((uint32_t)0x1ff)
+#define MS_UART_RXDATA_REG_MAX_VALUE	((uint32_t)0x1FF)
 
-#define AUCOHL_UART_TXE_FLAG	0x1
-#define AUCOHL_UART_RXF_FLAG	0x2
-#define AUCOHL_UART_TXB_FLAG	0x4
-#define AUCOHL_UART_RXA_FLAG	0x8
-#define AUCOHL_UART_BRK_FLAG	0x10
-#define AUCOHL_UART_MATCH_FLAG	0x20
-#define AUCOHL_UART_FE_FLAG	0x40
-#define AUCOHL_UART_PRE_FLAG	0x80
-#define AUCOHL_UART_OR_FLAG	0x100
-#define AUCOHL_UART_RTO_FLAG	0x200
+#define MS_UART_TXDATA_REG_TXDATA_BIT	((uint32_t)0)
+#define MS_UART_TXDATA_REG_TXDATA_MASK	((uint32_t)0x1ff)
+#define MS_UART_TXDATA_REG_MAX_VALUE	((uint32_t)0x1FF)
 
-typedef struct _AUCOHL_UART_TYPE_ {
+#define MS_UART_PR_REG_PR_BIT	((uint32_t)0)
+#define MS_UART_PR_REG_PR_MASK	((uint32_t)0xffff)
+#define MS_UART_PR_REG_MAX_VALUE	((uint32_t)0xFFFF)
+
+#define MS_UART_CTRL_REG_EN_BIT	((uint32_t)0)
+#define MS_UART_CTRL_REG_EN_MASK	((uint32_t)0x1)
+#define MS_UART_CTRL_REG_TXEN_BIT	((uint32_t)1)
+#define MS_UART_CTRL_REG_TXEN_MASK	((uint32_t)0x2)
+#define MS_UART_CTRL_REG_RXEN_BIT	((uint32_t)2)
+#define MS_UART_CTRL_REG_RXEN_MASK	((uint32_t)0x4)
+#define MS_UART_CTRL_REG_LPEN_BIT	((uint32_t)3)
+#define MS_UART_CTRL_REG_LPEN_MASK	((uint32_t)0x8)
+#define MS_UART_CTRL_REG_GFEN_BIT	((uint32_t)4)
+#define MS_UART_CTRL_REG_GFEN_MASK	((uint32_t)0x10)
+#define MS_UART_CTRL_REG_MAX_VALUE	((uint32_t)0x1F)
+
+#define MS_UART_CFG_REG_WLEN_BIT	((uint32_t)0)
+#define MS_UART_CFG_REG_WLEN_MASK	((uint32_t)0xf)
+#define MS_UART_CFG_REG_STP2_BIT	((uint32_t)4)
+#define MS_UART_CFG_REG_STP2_MASK	((uint32_t)0x10)
+#define MS_UART_CFG_REG_PARITY_BIT	((uint32_t)5)
+#define MS_UART_CFG_REG_PARITY_MASK	((uint32_t)0xe0)
+#define MS_UART_CFG_REG_TIMEOUT_BIT	((uint32_t)8)
+#define MS_UART_CFG_REG_TIMEOUT_MASK	((uint32_t)0x3f00)
+#define MS_UART_CFG_REG_MAX_VALUE	((uint32_t)0x3FFF)
+
+#define MS_UART_MATCH_REG_MATCH_BIT	((uint32_t)0)
+#define MS_UART_MATCH_REG_MATCH_MASK	((uint32_t)0x1ff)
+#define MS_UART_MATCH_REG_MAX_VALUE	((uint32_t)0x1FF)
+
+#define MS_UART_RX_FIFO_LEVEL_REG_LEVEL_BIT	((uint32_t)0)
+#define MS_UART_RX_FIFO_LEVEL_REG_LEVEL_MASK	((uint32_t)0xf)
+#define MS_UART_RX_FIFO_LEVEL_REG_MAX_VALUE	((uint32_t)0xF)
+
+#define MS_UART_RX_FIFO_THRESHOLD_REG_THRESHOLD_BIT	((uint32_t)0)
+#define MS_UART_RX_FIFO_THRESHOLD_REG_THRESHOLD_MASK	((uint32_t)0xf)
+#define MS_UART_RX_FIFO_THRESHOLD_REG_MAX_VALUE	((uint32_t)0xF)
+
+#define MS_UART_RX_FIFO_FLUSH_REG_FLUSH_BIT	((uint32_t)0)
+#define MS_UART_RX_FIFO_FLUSH_REG_FLUSH_MASK	((uint32_t)0x1)
+#define MS_UART_RX_FIFO_FLUSH_REG_MAX_VALUE	((uint32_t)0x1)
+
+#define MS_UART_TX_FIFO_LEVEL_REG_LEVEL_BIT	((uint32_t)0)
+#define MS_UART_TX_FIFO_LEVEL_REG_LEVEL_MASK	((uint32_t)0xf)
+#define MS_UART_TX_FIFO_LEVEL_REG_MAX_VALUE	((uint32_t)0xF)
+
+#define MS_UART_TX_FIFO_THRESHOLD_REG_THRESHOLD_BIT	((uint32_t)0)
+#define MS_UART_TX_FIFO_THRESHOLD_REG_THRESHOLD_MASK	((uint32_t)0xf)
+#define MS_UART_TX_FIFO_THRESHOLD_REG_MAX_VALUE	((uint32_t)0xF)
+
+#define MS_UART_TX_FIFO_FLUSH_REG_FLUSH_BIT	((uint32_t)0)
+#define MS_UART_TX_FIFO_FLUSH_REG_FLUSH_MASK	((uint32_t)0x1)
+#define MS_UART_TX_FIFO_FLUSH_REG_MAX_VALUE	((uint32_t)0x1)
+
+
+#define MS_UART_TXE_FLAG	((uint32_t)0x1)
+#define MS_UART_RXF_FLAG	((uint32_t)0x2)
+#define MS_UART_TXB_FLAG	((uint32_t)0x4)
+#define MS_UART_RXA_FLAG	((uint32_t)0x8)
+#define MS_UART_BRK_FLAG	((uint32_t)0x10)
+#define MS_UART_MATCH_FLAG	((uint32_t)0x20)
+#define MS_UART_FE_FLAG	((uint32_t)0x40)
+#define MS_UART_PRE_FLAG	((uint32_t)0x80)
+#define MS_UART_OR_FLAG	((uint32_t)0x100)
+#define MS_UART_RTO_FLAG	((uint32_t)0x200)
+
+
+          
+/******************************************************************************
+* Typedefs and Enums
+******************************************************************************/
+          
+typedef struct _MS_UART_TYPE_ {
 	__R 	RXDATA;
 	__W 	TXDATA;
 	__W 	PR;
 	__W 	CTRL;
 	__W 	CFG;
-	__W 	FIFOCTRL;
-	__R 	FIFOS;
+	__R 	reserved_0[2];
 	__W 	MATCH;
-	__R 	reserved[952];
-	__W 	icr;
-	__R 	ris;
-	__RW	im;
-	__R 	mis;
-} AUCOHL_UART_TYPE;
+	__R 	reserved_1[16248];
+	__R 	RX_FIFO_LEVEL;
+	__W 	RX_FIFO_THRESHOLD;
+	__W 	RX_FIFO_FLUSH;
+	__R 	reserved_2[1];
+	__R 	TX_FIFO_LEVEL;
+	__W 	TX_FIFO_THRESHOLD;
+	__W 	TX_FIFO_FLUSH;
+	__R 	reserved_3[57];
+	__RW	IM;
+	__R 	MIS;
+	__R 	RIS;
+	__W 	IC;
+	__W 	GCLK;
+} MS_UART_TYPE;
+
+typedef struct _MS_UART_TYPE_ *MS_UART_TYPE_PTR;     // Pointer to the register structure
+
+  
+/******************************************************************************
+* Function Prototypes
+******************************************************************************/
+
+
+
+/******************************************************************************
+* External Variables
+******************************************************************************/
+
+
+
 
 #endif
 
+/******************************************************************************
+* End of File
+******************************************************************************/
+          
+          
